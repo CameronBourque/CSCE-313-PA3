853204ece338198d8355cbd4ed1aa7f2fbb1e27c
FrozenLion <28263454+FrozenLion@users.noreply.github.com>
2020-02-28 11:17:20 -0600

    Updated README and added example code from in class

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # CSCE-313-PA3
+This program is for emulating a terminal with the following functionality:
+
+running commands
+redirection
+piping
+running background processes
+
+If I have time before the due date I will also include
+
+$ variable support
--- shell.cpp
+++ shell.cpp
@@ -5,17 +5,42 @@
 #include <sstream>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdlib.h>
 
 using namespace std;
 
 #define PROMPT ("shell:")
 #define MODE ("$")
+#define HOME_DIR ("/home/")
+#define O_OPEN_REDIR_W (O_CREAT | O_WRONLY | O_TRUNC)
+#define S_OPEN_REDIR_W (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+
+void convertPath(string &ret, string path, string home);
+bool isOp(string str);
+void processInput(vector<vector<string> > &cmdls, string &pwd, string &oldpwd);
 
 int main(){
+    //set up global variables
     string path = "";
     string pwd = "~";
     string oldpwd = "";
     bool running = true;
+    vector<int> pidList;
+    int child = 0;
+    string home = HOME_DIR + getenv("USER");
+    
+    //set up terminal base
+    string newpwd;
+    convertPath(newpwd, pwd, home);
+    cout << "new pwd: " << newpwd << endl;
+    int cret = chdir(newpwd.c_str()); //sets curDir to pwd
+    if(cret){
+        cout << "directory change failed on init" << endl;
+        return 0;
+    }
+    
 
     while(true){
         //prompt user
@@ -35,12 +60,7 @@ int main(){
         vector<vector<string> > cmdls;
         vector<string> temp;
         cmdls.push_back(temp);
-        /*
-            Ideas:
-                Separate all the strings by pipes and then further by
-                    redirection and continue down to quotes and then 
-                    spaces
-        */
+        
         int quote = -1;
         int tickq = -1;
         int whitespace = 0;
@@ -74,12 +94,9 @@ int main(){
             else if(isspace(input[i])){
                 if(whitespace > -1 && i > whitespace){
                     cmdls[process].push_back(input.substr(whitespace, i - whitespace));
-                    whitespace = i+1;
                 }
-                else{
-                    cout << "ws:: setting ws to " << i+1 << endl;
-                    whitespace = i+1;
-                }
+                
+                whitespace = i+1;
             }
             //check for operator (these don't apply in quotes) (these set the whitespace)
             else if(quote == -1 && (input[i] == '|' || input[i] == '<' || input[i] == '>' || input[i] == '&')){
@@ -87,14 +104,18 @@ int main(){
                 if(whitespace != -1 && i > whitespace){
                     cmdls[process].push_back(input.substr(whitespace,i - whitespace));
                 }
+                //operator will exist at start of vector
+                cmdls[process].push_back(input.substr(i, 1));
+                
+                //add next process space in
                 process++;
                 vector<string> temp;
                 cmdls.push_back(temp);
-                //operator will exist at start of vector
-                cmdls[process].push_back(input.substr(i, 1));
+                
                 //next string could exist immediately after
                 whitespace = i+1;
             }
+            //if at end of string get whatever string hassn't been added
             if(i+1 == input.size()){
                 if(quote > -1){
                     cmdls[process].push_back(input.substr(quote, i+1 - quote));
@@ -108,8 +129,15 @@ int main(){
             }
         }
 
-        //Grammar has been parsed
+        //remove any empty process
+        if(cmdls[cmdls.size()-1].size() == 0){
+            cmdls.pop_back();
+        }
 
+        //process the input and execute
+        processInput(cmdls, pwd, oldpwd);
+
+        //for debugging purposes
         for(int i = 0; i < cmdls.size(); i++){
             cout << "process " << i << endl;
             for(int j = 0; j < cmdls[i].size(); j++){
@@ -117,6 +145,70 @@ int main(){
             }
             cout << endl;
         }
-        
+    }
+}
+
+void convertPath(string &ret, string path, string home){
+    char buf[path.size() + home.size()-1];
+
+    bool squigle = false;
+    for(int i = 0; i < path.size()+home.size()-1; i++){
+        if(path[i] == '~'){
+            if(squigle){
+                buf[i] = path[i];
+            }
+            else{
+                squigle = true;
+                strcpy(buf+i, home.c_str());
+                i += home.size();
+            }
+        }
+        else{
+            buf[i] = path[i];
+        }
+    }
+
+    ret = buf;
+}
+
+bool isOperator(string str){
+    if(str.size() != 1) { return false; }
+
+    return str[0] == '|' || str[0] == '>' || str[0] == '<' || str[0] == '&';
+}
+
+void processInput(vector<vector<string> > &cmdls, string &pwd, string &oldpwd){
+//    char* args[] = {"ls", "-l", NULL};
+//    execvp("ls", args);
+
+    //loop through the processes
+    for(int p = 0; p < cmdls.size(); p++){
+        //FILL (fd setting)
+
+        int pid = fork();
+        if(!pid){}
+        else{}
+    }
+    //FILL
+}
+
+void examples(){
+    //THIS IS "ls -l -a > foo.txt" (WRITE TO FILE)
+    int fd = open("foo.txt", O_OPEN_REDIR_W, S_OPEN_REDIR_W);
+    if(!fork()){
+        dup2(fd, 1); //overwrites the stdout with the new file
+        execlp ("ls", "ls", "-l", "-a", NULL); //now execute
+    }
+
+    //THIS IS "ls -l | grep soda"
+    int fds[2];
+    pipe(fds);
+    if(!fork()){
+        dup2(fds[1], 1); //redirect stdout to pipeout
+        execlp("ls", "ls", "-l", NULL);
+    }
+    else{
+        dup2(fds[0], 0); //redirect stdin to pipein
+        execlp("grep", "grep", "soda", NULL);
     }
 }
